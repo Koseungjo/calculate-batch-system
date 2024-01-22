c024ec7945dea47fa92966119ce96192f1135366
imokoreacorp <imokoreacorp@gmail.com>
2024-01-22 09:49:28 +0900

    first commit

---
+++ src/main/java/com/example/calculatebatchsystem/CalculateBatchSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.calculatebatchsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculateBatchSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculateBatchSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/calculatebatchsystem/CalculateBatchSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calculatebatchsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculateBatchSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
